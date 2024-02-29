feaae00c6d0eda7410bb29dec5c85b90240912ba
andysigg123 <110823661+andysigg123@users.noreply.github.com>
2024-02-29 10:55:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ for(let i=0;i <botoes.length;i++){
 
        for(let j=0;j<botoes.length;j++) {
          botoes[j].classList.remove("ativo");
+         textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+        const textos = document.querySelectorAll(".aba-conteudo");
    }
   }
+  const contadores = document.querySelectorAll(".contador");
+  contadores[0].textContent = "Contagem regressiva";
