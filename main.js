c8e87d0367b9bcb67130169a6e88000e182eaf21
andysigg123 <110823661+andysigg123@users.noreply.github.com>
2024-03-26 09:49:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,12 @@ for(let i=0;i <botoes.length;i++){
   contadores[0].textContent = tempoObjetivo1;
 
   contadores[0].textContent = "Contagem regressiva";
+
+  function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    return horas;
+    }
