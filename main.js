0347b54825ce08175e6732dd7dd762fbb4b95d78
andysigg123 <110823661+andysigg123@users.noreply.github.com>
2024-04-02 09:07:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,9 +15,34 @@ for(let i=0;i <botoes.length;i++){
   }
   const contadores = document.querySelectorAll(".contador");
   const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-  contadores[0].textContent = tempoObjetivo1;
+  const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+  const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+  const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-  contadores[0].textContent = "Contagem regressiva";
+  const tempos =
+  function atualizaCronometro(){
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+    } }
+
+  function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000); }
+    comecaCronometro();
+
+  contadores[0].textContent = calculaTempo(tempos[0]);
+  contadores[1].textContent = calculaTempo(tempos[1]);
+  contadores[2].textContent = calculaTempo(tempos[2]);
+  contadores[3].textContent = calculaTempo(tempos[3]);
+  
+  contadores[0].textContent = tempoObjetivo1;
+  contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+  contadores[1].textContent = tempoObjetivo2 - tempoAtual;
+  contadores[2].textContent = tempoObjetivo3 - tempoAtual;
+  contadores[3].textContent = tempoObjetivo4 - tempoAtual;
+
+
+contadores[0].textContent = "Contagem regressiva";
 
   function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
